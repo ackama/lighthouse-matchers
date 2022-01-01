89794519dbb1bb16cc03238f8b062dc0684a82ae
Josh McArthur <joshua.mcarthur@gmail.com>
2022-01-01 21:46:42 +1300

    Updates rubocop and makes necessary style changes

--- spec/lighthouse/matchers/rspec_spec.rb
+++ spec/lighthouse/matchers/rspec_spec.rb
@@ -88,7 +88,7 @@ RSpec.describe 'pass_lighthouse_audit matcher' do
       before { Lighthouse::Matchers.chrome_flags = '--headless --no-sandbox' }
 
       it 'executes the expected command' do
-        command = expected_command + " --chrome-flags='--headless --no-sandbox'"
+        command = "#{expected_command} --chrome-flags='--headless --no-sandbox'"
 
         expect(runner).to receive(:call)
           .with(command)
@@ -102,7 +102,7 @@ RSpec.describe 'pass_lighthouse_audit matcher' do
       before { Lighthouse::Matchers.chrome_flags = %w[headless no-sandbox] }
 
       it 'executes the expected command' do
-        command = expected_command + " --chrome-flags='--headless --no-sandbox'"
+        command = "#{expected_command} --chrome-flags='--headless --no-sandbox'"
 
         expect(runner).to receive(:call)
           .with(command)
