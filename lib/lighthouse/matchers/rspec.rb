6f25211cd58479081906d915468bb878501ce793
Gareth Jones <jones258@gmail.com>
2024-06-22 08:34:53 +1200

    feat: write audit results to file

--- lib/lighthouse/matchers/rspec.rb
+++ lib/lighthouse/matchers/rspec.rb
@@ -9,21 +9,36 @@ RSpec::Matchers.define :pass_lighthouse_audit do |audit, args = {}|
   score ||= args.fetch(:score, Lighthouse::Matchers.minimum_score)
 
   match do |target|
-    audit_service = AuditService.new(url(target), audit, score)
-
-    @measured_score = audit_service.measured_score
-
-    audit_service.passing_score?
+    @audit_service = AuditService.new(url(target), audit, score)
+    @audit_service.passing_score?
   end
 
   failure_message do |target|
     <<~FAIL
       expected #{url(target)} to pass Lighthouse #{audit} audit
-      with a minimum score of #{score}, but only scored #{@measured_score.to_i}
+      with a minimum score of #{score}, but only scored #{@audit_service.measured_score}
+
+     Full report:
+      #{save_audit_results}
+
+      To view this report, load this file into https://googlechrome.github.io/lighthouse/viewer/
     FAIL
   end
 
   private
+
+  def save_audit_results
+    @audit_results_path ||= write_file(JSON.pretty_generate(@audit_service.results))
+  end
+
+  def write_file(body)
+    path = File.join(Lighthouse::Matchers.results_directory, "#{Digest::SHA1.hexdigest(body)}.json")
+
+    FileUtils.mkdir_p(Lighthouse::Matchers.results_directory)
+    File.write(path, body)
+
+    path
+  end
 
   def url(target)
     target.respond_to?(:current_url) ? target.current_url : target
