fbbc0359fd306758690e37d500138a0dd4ffd100
Gareth Jones <jones258@gmail.com>
2024-06-23 09:28:04 +1200

    feat: error if category is not found

--- lib/lighthouse/audit_service.rb
+++ lib/lighthouse/audit_service.rb
@@ -5,6 +5,8 @@ require 'stringio'
 
 # Compares a url's actual score to the expected score.
 class AuditService
+  class Error < StandardError; end
+
   def initialize(url, audit, score)
     @url = url
     @audit = audit
@@ -20,7 +22,7 @@ class AuditService
   end
 
   def measured_score
-    results.dig('categories', @audit.to_s, 'score') * 100
+    category['score'] * 100
   end
 
   def run_warnings
@@ -32,6 +34,14 @@ class AuditService
   end
 
   private
+
+  def category
+    category = results.dig('categories', @audit.to_s)
+
+    raise Error, "Category '#{@audit}' not found in Lighthouse results - maybe it was removed?" if category.nil?
+
+    category
+  end
 
   def opts
     "'#{@url}'".tap do |builder|
